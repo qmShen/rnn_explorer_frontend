a14c49062ec094f532aabfe34d97b65fe9d38c43
YuzheJiang <345854890@qq.com>
2019-03-13 17:35:36 +0800

    scatter_plot

--- src/components/visView/ScatterplotView.vue
+++ src/components/visView/ScatterplotView.vue
@@ -1,18 +1,55 @@
 <template>
-  <!--<svg class = 'vis_container'></svg>-->
   <div>
+    <svg></svg>
+    <div class="block">
+      <span class="demonstration"></span>
+      <el-slider v-model="value" :show-tooltip="false"></el-slider>
+    </div>
   </div>
 </template>
 
 <script>
+
+  import Scatter from "./scatterplot.js"
   export default {
     name: "scatterplotView",
-    props:['title','scatter_data'],
+    props:['scatter_data'],
+
+    mounted:function(){
+      this.scatterplot = new Scatter(this.$el);
+    },
+
     watch:{
+
+      "value": function(val){
+        if(val !=null){
+          this.move(val)
+        }
+      },
+
       scatter_data: function(val){
-        console.log('oppo', val);
+        if(val != null){
+          this.update_scatter(val)
+        }
       }
-    }
+    },
+
+    methods:{
+      update_scatter(data){
+        this.scatterplot.update_render(data)
+      },
+
+      move(data){
+        this.scatterplot.move_render(data)
+      }
+    },
+
+    data() {
+      return {
+        value:0
+      }
+    },
+
   }
 </script>
 
